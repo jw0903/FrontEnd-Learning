4dee95d649b62a3c0fe8e9f80b3e8c7c38c880b5
lin559 <scau_ljw@126.com>
2019-06-27 02:05:34 +0800

    [MOD]

--- demo.js
+++ demo.js
@@ -35,3 +35,61 @@ function observe(data){
 // data.name = 'luan';
 // data.age = 12;
 // https://cn.vuejs.org/v2/guide/list.html#注意事项
+
+
+// 使用Proxy示例
+const createProxy = data => {
+	if (typeof data === 'object' && data.toString() === '[object Object]') {
+		for (let k in data) {
+			if (typeof data[k] === 'object'){
+				// 如果是对象的话，类似数组，采用一下方式
+				defineObject(data, k,data[k]);
+			} else {
+				// 如果是其他类型：数字/字符串 则采用以下方式
+				defineBasic(data, k, data[k]);
+			}
+		}
+	}
+}
+
+function defineObject(obj, key, value) {
+	createProxy(value);
+	obj[key] = new Proxy(value, {
+		set(target, property, val, receiver) {
+			if (property !== 'length') {
+				console.log('set %s to %o', property, val);
+			}
+			return Reflect.set(target, property, val, receiver);
+		}
+	})
+}
+
+function defineBasic(obj, key, value) {
+	Object.defineProperty(obj, key, {
+		enumerable: true,
+		configurable: false,
+		get () {
+			return value
+		},
+		set(newVal) {
+			if (value === newVal) return;
+			console.log(`发现${key} 属性 ${value} ---> ${newVal}`)
+			value = newVal;
+		}
+	})
+}
+
+let data = {
+	name: 'jiang',
+	userInfo: {
+		gender: 0,
+		movies: []
+	},
+	list: []
+};
+createProxy(data);
+data.name = 'solo';
+data.userInfo.movies.push('星际穿越');
+data.list.push(100);
+data.list[0] = 'THE FIRST VALUE';
+
